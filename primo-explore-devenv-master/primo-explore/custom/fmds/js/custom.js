b66e4cb993380053f26e8a78eec581405c3ea6f1
fm santos <santos.fmd@gmail.com>
2017-08-30 15:48:08 +1000

    wsu commit 20170830

--- primo-explore-devenv-master/primo-explore/custom/fmds/js/custom.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    "use strict";
-    'use strict';
-
-    var app = angular.module('viewCustom', ['angularLoad'], function ($compileProvider) {
-        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|data):/);
-    });
-
-})();
